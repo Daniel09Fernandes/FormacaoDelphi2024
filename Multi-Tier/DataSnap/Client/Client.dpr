65481bc78fea2b1aa0d9cb702762ba24176b8330
Gustavo Mena Barreto <56980191+gustavomenabarreto@users.noreply.github.com>
2024-06-17 15:55:50 -0300

    Add files via upload

---
+++ Multi-Tier/DataSnap/Client/Client.dpr
@@ -0,0 +1,20 @@
+program Client;
+
+uses
+  Vcl.Forms,
+  FormPrincipalClient in 'FormPrincipalClient.pas' {Form3},
+  ClientClassesUnit1 in 'ClientClassesUnit1.pas',
+  ClientModuleUnit1 in 'ClientModuleUnit1.pas' {ClientModule1: TDataModule},
+  ClassePessoa in '..\Server\ClassePessoa.pas',
+  Pkg.Json.DTO in 'Pkg.Json.DTO.pas',
+  RootUnit in 'RootUnit.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TClientModule1, ClientModule1);
+  Application.Run;
+end.
---
+++ Multi-Tier/DataSnap/Client/ClientModuleUnit1.dfm
@@ -0,0 +1,12 @@
+object ClientModule1: TClientModule1
+  Height = 407
+  Width = 623
+  PixelsPerInch = 144
+  object DSRestConnection1: TDSRestConnection
+    Port = 8080
+    LoginPrompt = False
+    Left = 72
+    Top = 60
+    UniqueId = '{615E61F0-F348-42A9-842F-CA54F99F9DA5}'
+  end
+end
